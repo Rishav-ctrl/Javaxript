3dfdb4813b42af58d47cb0f670f47a9c1f1faf52
Shrawak Hack <shrawakagni@gmail.com>
2025-04-18 03:11:26 +0000

    Conversion Confusion of Datatypes in Js

---
+++ basic1/conversion.js
@@ -0,0 +1,20 @@
+let score = "23"
+
+console.log(typeof(score))
+
+let newsc = Number(score)
+
+console.log(typeof(newsc))
+
+//The straight number like 23 is easily converted to number
+//but if there is other than number like 33abs shrawak.., then it will give NaN -> Not a Number 
+// but the type of NaN is Number..
+
+let isloggedin = -1
+
+let boolconvert = Boolean(isloggedin)
+
+console.log(boolconvert)
+console.log(typeof(boolconvert))
+
+//we can also convert the boolean value
